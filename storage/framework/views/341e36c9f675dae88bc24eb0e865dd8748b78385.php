dbe1bfaac47dd4503d114141b456b0a7656ba452
tawar.kapil@gmail.com <tawar.kapil@gmail.com>
2024-12-26 20:00:30 +0530

    Group add

---
+++ storage/framework/views/341e36c9f675dae88bc24eb0e865dd8748b78385.php
@@ -0,0 +1,226 @@
+ 
+<?php $__env->startSection('content'); ?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Our Members</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a class="text-muted" href="<?php echo e(url('admin/dashboard')); ?>">Home</a></li>
+              <li class="breadcrumb-item active">Our Members</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title float-left">Our Members</h3>
+                <div class="float-right">
+                    <button type="button" class="btn bg-gradient-primary addNewBtn"><i class="fa fa-plus"></i> Add New</button>
+                    <button type="button" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" title="Filter" class="btn waves-effect waves-light btn-warning"><i class="fa fa-filter"></i> Filter</button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div class="row collapse" id="collapseExample">
+                   <div class="col-lg-12">
+                      <div class="filter-main-box">
+                         <form name="filterFrm" id="filterFrm">
+                            <div class="row">
+                               <div class="col-lg-4 col-md-6">
+                                  <div class="form-group">
+                                     <label>Start Date</label>
+                                     <input class="form-control custom-date-pickeer" type="text" name="srch_start_date" id="srch_start_date" autocomplete="off">
+                                  </div>
+                               </div>
+                               <div class="col-lg-4 col-md-6">
+                                  <div class="form-group">
+                                     <label>End Date</label>
+                                     <input class="form-control custom-date-pickeer" type="text" name="srch_end_date" id="srch_end_date" autocomplete="off">
+                                  </div>
+                               </div>
+                               <div class="col-lg-4 col-md-6">
+                                  <div style="margin-top: 32px;">
+                                     <a class="btn btn-secondary text-light mr-1 resetBtn" type="button">Reset</a>
+                                     <a class="btn btn-info text-light searchBtn" type="submit">Go</a>
+                                  </div>
+                               </div>
+                            </div>
+                         </form>
+                      </div>
+                   </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <table class="table table-bordered" style="width:100%;" id="custom-ajax-tbl">
+                          <thead>
+                           <tr>
+                             <th>#</th>
+                             <!-- <th>Image</th> -->
+                             <th>Name</th>
+                             <th>Destination</th>
+                             <th>Description</th>
+                             <th>Status</th>
+                             <th class="all">Date</th>
+                             <th class="all">Action</th>
+                          </tr>
+                          </thead>
+                          <tbody>
+                          </tbody>
+                        </table>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+
+  <div class="modal fade" id="viewdescriptionMdl">
+   <div class="modal-dialog modal-md">
+      <div class="modal-content">
+         <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+         </div>
+         <div class="modal-body">
+         </div>
+      </div>
+   </div>
+</div>
+
+<!-- /.modal -->
+<div class="modal fade" id="submitFrmMdl">
+   <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+         <form name="submitFrm" id="submitFrm">
+            <div class="modal-header bg-primary">
+               <h4 class="modal-title float-left" id="page_headline"> Add New</h4>
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+               <div class="row">
+                <div class="col-lg-3">
+                 <div class="form-group">
+                    <label for="cover_image">Cover Image <span class="required text-danger">*</span></label><br/>
+                    <input type="file" id="cover_image" name="cover_image">
+                 </div>
+              </div>
+              <div class="col-lg-2">
+                 <div class="imgDisplayBx"></div>
+              </div>
+               <div class="col-lg-12">
+                  <div class="form-group">
+                     <label for="name">Name <span class="required text-danger">*</span></label>
+                     <input type="text" class="form-control" id="name" name="name">
+                  </div>
+               </div>
+
+               <div class="col-lg-6">
+                  <div class="form-group">
+                     <label for="email">Email <span class="required text-danger">*</span></label>
+                     <input type="text" class="form-control" id="email" name="email">
+                  </div>
+               </div>
+               <div class="col-lg-6">
+                  <div class="form-group">
+                     <label for="destination">Destination <span class="required text-danger">*</span></label>
+                     <?php echo Form::select('destination', [ '' => 'Please Select'] + $destinations, null, ['id' => 'destination', 'class' => 'form-control']); ?>
+
+                  </div>
+               </div>
+
+               <div class="col-lg-6">
+                  <div class="form-group">
+                     <label for="designation">Designation <span class="required text-danger">*</span></label>
+                     <input type="text" class="form-control" id="designation" name="designation">
+                  </div>
+               </div>
+               <div class="col-lg-6">
+                  <div class="form-group">
+                     <label for="status">Status <span class="required text-danger">*</span></label>
+                     <?php echo Form::select('status', [ '' => 'Please Select', 1 => 'Active', 0 => 'Inactive'], null, ['id' => 'status', 'class' => 'form-control']); ?>
+
+                  </div>
+               </div>
+               <div class="col-lg-12">
+                  <div class="form-group">
+                     <label for="description">Description <span class="required text-danger">*</span></label>
+                     <textarea class="form-control" rows="5" id="description" name="description"></textarea>
+                  </div>
+               </div>
+
+               </div>
+               
+            </div>
+            <div class="modal-footer">
+               <button type="button" class="btn btn-default pull-left" data-dismiss="modal"> Close </button>
+               <button type="submit" id="submitBtn" name="submitBtn" class="btn btn-success"> Save </button>
+            </div>
+         </form>
+      </div>
+      <!-- /.modal-description -->
+   </div>
+   <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
+<div class="modal fade text-left" id="confirm_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel111" aria-hidden="true">
+   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+      <div class="modal-content">
+         <div class="modal-header bg-primary">
+            <h5 class="modal-title white" id="myModalLabel111">Change Status</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <i class="bx bx-x"></i>
+            </button>
+         </div>
+         <div class="modal-body">
+           <div class="modal-content-value"></div>
+         </div>
+         <div class="modal-footer text-right">
+            <button data-dismiss="modal" class="btn btn-danger">CANCEL</button> 
+            <button class="btn btn-success confirm_status">CONFIRM</button>
+         </div>
+      </div>
+   </div>
+</div>
+
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('styles'); ?>
+<link rel="stylesheet" type="text/css" href="<?php echo e(url('public/panel/assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')); ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo e(url('public/panel/assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')); ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo e(url('public/panel/assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')); ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo e(url('public/panel/assets/plugins/daterangepicker/daterangepicker.css')); ?>" />
+<style type="text/css">
+   .cke_toolbar_last{
+      display: none !important;
+   }
+   .dataTables_scrollHeadInner{
+      width: 100%;
+   }
+</style>
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('scripts'); ?> 
+<script src="<?php echo e(url('public/panel/assets/plugins/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
+<script src="<?php echo e(url('public/panel/assets/plugins/datatables/jquery.dataTables.min.js')); ?>"></script>
+<script src="<?php echo e(url('public/panel/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')); ?>"></script>
+<script src="<?php echo e(url('public/panel/assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')); ?>"></script>
+<script src="<?php echo e(url('public/panel/assets/plugins/daterangepicker/daterangepicker.js')); ?>"></script>
+<!-- <script src="https://cdn.ckeditor.com/4.11.1/full-all/ckeditor.js"></script> -->
+<script type="text/javascript" src="<?php echo e(url('public/panel/custom/our-member/index.js')); ?>"></script>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('admin.layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/elective/resources/views/admin/our-member/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d131c3206ad54b7f4cd666754625c8aa1500904e.php
@@ -0,0 +1,127 @@
+ 
+<?php $__env->startSection('content'); ?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">User Transactions</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a class="text-muted" href="<?php echo e(url('admin/dashboard')); ?>">Home</a></li>>
+              <li class="breadcrumb-item active">User Transactions</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title float-left">User Transactions</h3>
+                <div class="float-right">
+                    <button type="button" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" title="Filter" class="btn waves-effect waves-light btn-warning"><i class="fa fa-filter"></i> Filter</button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div class="row collapse" id="collapseExample">
+                   <div class="col-lg-12">
+                      <div class="filter-main-box">
+                         <form name="filterFrm" id="filterFrm">
+                            <div class="row">  
+                                 <div class="col-lg-4 col-md-6">
+                                    <div class="form-group">
+                                         <label>Membership</label>
+                                         <?php echo Form::select('srch_plan_type', ['' => 'Please Select'] + $plans, null, ['id' => 'srch_plan_type', 'class' => 'form-control']); ?>
+
+                                    </div>
+                                </div> 
+                                <div class="col-lg-4 col-md-6">
+                                    <div class="form-group">
+                                         <label>Start Date</label>
+                                         <input class="form-control custom-date-pickeer" type="text" name="srch_start_date" id="srch_start_date" autocomplete="off">
+                                    </div>
+                                </div>
+                                
+                                <div class="col-lg-4 col-md-6">
+                                    <div class="form-group">
+                                         <label>End Date</label>
+                                         <input class="form-control custom-date-pickeer" type="text" name="srch_end_date" id="srch_end_date" autocomplete="off">
+                                    </div>
+                                </div>                     
+                                <div class="col-lg-12">
+                                    <div class="mt-3 float-right">
+                                        <a class="btn btn-secondary text-light mr-1 resetBtn" type="button">Reset</a>
+                                        <a class="btn btn-info text-light searchBtn" type="submit">Go</a>
+                                    </div>
+                                </div>
+                            </div>  
+                         </form>
+                      </div>
+                   </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                      <table class="table table-bordered" id="custom-ajax-tbl">
+                         <thead>
+                           <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th class="none">Email/Phone  </th>
+                            <th>Plan Title</th>
+                            <th>Transaction Id</th>
+                            <th>Amount</th>
+                            <th>Payment Mode</th>
+                            <th>Status</th>
+                            <th  class="all">Payment Date</th>
+                            <th  class="all">Action</th>
+                         </tr>
+                         <tbody></tbody>
+                      </table>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+<div class="modal fade" id="viewContentMdl">
+   <div class="modal-dialog modal-md">
+      <div class="modal-content">
+         <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+         </div>
+         <div class="modal-body">
+         </div>
+      </div>
+   </div>
+</div>
+
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('styles'); ?>
+<link rel="stylesheet" type="text/css" href="<?php echo e(url('public/panel/assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')); ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo e(url('public/panel/assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')); ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo e(url('public/panel/assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')); ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo e(url('public/panel/assets/plugins/daterangepicker/daterangepicker.css')); ?>" />
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('scripts'); ?> 
+<script src="<?php echo e(url('public/panel/assets/plugins/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
+<script src="<?php echo e(url('public/panel/assets/plugins/datatables/jquery.dataTables.min.js')); ?>"></script>
+<script src="<?php echo e(url('public/panel/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')); ?>"></script>
+<script src="<?php echo e(url('public/panel/assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')); ?>"></script>
+<script src="<?php echo e(url('public/panel/assets/plugins/daterangepicker/daterangepicker.js')); ?>"></script>
+<script type="text/javascript" src="<?php echo e(url('public/panel/custom/usertransaction/index.js')); ?>"></script>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('admin.layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/elective/resources/views/admin/usertransaction/index.blade.php ENDPATH**/ ?>
